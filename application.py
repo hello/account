a245971a7fecfe9d680be38d64798665ce48c03e
longd3 <izzy3ds@gmail.com>
2015-03-11 16:31:25 -0700

    Change config for local dev convenience

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from app import application
-
+import os
 if __name__ == '__main__':
-    application.run(host='0.0.0.0',debug=application.config['DEBUG'])
+    port = int(os.environ['PORT'])
+    application.run(host='0.0.0.0',debug=application.config['DEBUG'], port=port)
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-APP_SETTINGS="app.config.ProductionConfig" python application.py
+#!/bin/bash
+APP_SETTINGS="app.config.Config" PORT=5000 python application.py
